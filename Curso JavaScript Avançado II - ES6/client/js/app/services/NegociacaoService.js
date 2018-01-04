9a41fc6f079234e347c69bc8aa23a81ceba9a83e
Uniliva Alves Pereira <uniliva-ap@hotmail.com>
2018-01-04 10:16:51 -0200

    Atividade 4 Isolando a complexidade em HttpService

--- Curso JavaScript Avançado II - ES6/client/js/app/services/NegociacaoService.js
+++ Curso JavaScript Avançado II - ES6/client/js/app/services/NegociacaoService.js
@@ -1,79 +1,41 @@
 class NegociacaoService {
+    constructor() {
+        this._http = new HttpService();
+    }
 
     obterNegociacaoSemana() {
         return new Promise((resolve, reject) => {
-            let xhr = new XMLHttpRequest();
-            xhr.open('GET', 'negociacoes/semana');
-            xhr.onreadystatechange = () => {
-                if (xhr.readyState == 4) {
-                    if (xhr.status == 200) {
-                        console.log(xhr.responseText);
-                        resolve(JSON.parse(xhr.responseText).map(obj => new Negociacao(obj.data, obj.quantidade, obj.valor)))
+            this._http.get('negociacoes/semana').then(negociacao => {
+                resolve(negociacao.map(obj => new Negociacao(obj.data, obj.quantidade, obj.valor)))
 
-                    } else {
-                        reject("Não foi possivel recuperar a lista de negociações");
-                    }
-                }
-
-            }
-            xhr.send();
-        });
+            }).catch(erro => {
+                console.log(erro);
+                reject("Não foi possivel recuperar a lista de negociações");
+            });
+        })
     }
     obterNegociacaoSemanaAnterior() {
+
         return new Promise((resolve, reject) => {
-            let xhr = new XMLHttpRequest();
-            //abrir uma conexão, passando o method e o a uri
-            xhr.open('GET', 'negociacoes/anterior');
-            //configurando
-            xhr.onreadystatechange = () => {
-                /*
-                    0: requisição ainda não iniciada
-                    1: conexão com o servidor estabelecida
-                    2: requisição recebida
-                     3: processando requisição
-                    4: requisição está concluída e a resposta está pronta
-                */
-                if (xhr.readyState == 4) {
-                    if (xhr.status == 200) {
-                        resolve(JSON.parse(xhr.responseText).map(obj => new Negociacao(obj.data, obj.quantidade, obj.valor)))
+            this._http.get('negociacoes/anterior').then(negociacao => {
+                resolve(negociacao.map(obj => new Negociacao(obj.data, obj.quantidade, obj.valor)))
 
-                    } else {
-                        console.log(xhr.responseText);
-                        reject("Não foi possivel recuperar a lista de negociações");
-                    }
-                }
-
-            }
-            //executando a operacao
-            xhr.send();
-        });
+            }).catch(erro => {
+                console.log(erro);
+                reject("Não foi possivel recuperar a lista de negociações");
+            });
+        })
     }
     obterNegociacaoSemanaRetrasada() {
-        return new Promise((resolve, reject) => {
-            let xhr = new XMLHttpRequest();
-            //abrir uma conexão, passando o method e o a uri
-            xhr.open('GET', 'negociacoes/retrasada');
-            //configurando
-            xhr.onreadystatechange = () => {
-                /*
-                    0: requisição ainda não iniciada
-                    1: conexão com o servidor estabelecida
-                    2: requisição recebida
-                     3: processando requisição
-                    4: requisição está concluída e a resposta está pronta
-                */
-                if (xhr.readyState == 4) {
-                    if (xhr.status == 200) {
-                        resolve(JSON.parse(xhr.responseText).map(obj => new Negociacao(obj.data, obj.quantidade, obj.valor)))
 
-                    } else {
-                        console.log(xhr.responseText);
-                        reject("Não foi possivel recuperar a lista de negociações");
-                    }
-                }
-            }
-            //executando a operacao
-            xhr.send();
-        });
+            return new Promise((resolve, reject) => {
+                this._http.get('negociacoes/retrasada').then(negociacao => {
+                    resolve(negociacao.map(obj => new Negociacao(obj.data, obj.quantidade, obj.valor)))
+    
+                }).catch(erro => {
+                    console.log(erro);
+                    reject("Não foi possivel recuperar a lista de negociações");
+                });
+            })
     }
 }
